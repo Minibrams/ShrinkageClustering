283e876355a54665d02f10020f328a3e8f5cc5f0
Minibrams <anders@brams.dk>
2019-03-07 23:04:02 +0100

    Added visualization, still clusters two isntead of three

--- shrinkage_clustering.py
+++ shrinkage_clustering.py
@@ -30,17 +30,35 @@ def random_cluster_matrix(shape):
 
 
 def remove_clusters(A, cluster_indeces): 
+    """
+    Removes the clusters (i.e. columns given by the indices) from a matrix A. 
+    """
     return np.delete(A, cluster_indeces, axis=1)
 
 
 def progress(current, max): 
+    """
+    Prints a progress bar given the current number of steps and the maximum/goal 
+    number of steps.
+    """
     prog = int(20 * (current / max))
-    print(f"[{''.join('=' for _ in range(prog))}>{''.join(' ' for _ in range(20 - prog - 1))}] ({(current / max) * 100 : 2.2f}%)", end='\r')
+    print(f"[{''.join('=' for _ in range(prog))}:{''.join(' ' for _ in range(20 - prog - 1))}] ({(current / max) * 100 : 2.2f}%)", end='\r')
 
 
+def plot(points, A, k): 
+    colors = ['r', 'g', 'b', 'k', 'm', 'y', 'black', 'purple', 'pink', 'azure']
+    plt.clf()
+    
+    for cluster in range(k): 
+        point_idxs = np.argwhere(A.T[cluster])
+        pts = [(x,y) for i, (x,y) in enumerate(points) if i in point_idxs]
+        if pts:
+            xs, ys = zip(*pts)
+            plt.plot(xs, ys, 'bo', color=colors[cluster])
+        
+    plt.pause(0.0001)
 
-
-def cluster(S, k=None, max_iter=100): 
+def cluster(S, k=None, max_iter=100, visualize=False, points=None): 
     """
     Uses Shrinkage clustering to provide a cluster assignments matrix given a 
     similarity matrix S. 
@@ -55,19 +73,23 @@ def cluster(S, k=None, max_iter=100):
     S_bar = 1 - 2 * S
     A = random_cluster_matrix((N, k))
 
+    if visualize:
+        if not points: 
+            raise ValueError("Cannot visualize clustering without points.")
+        plt.ion()
+
     for _i in range(max_iter): 
         # Remove empty clusters
         empty_columns = [i for i, c in enumerate(A.T) if sum(c) == 0]
         A = remove_clusters(A, empty_columns)
-        k = len(A[0]) # Adjust number of clusters
+        k = len(A[0])  # Adjust number of clusters
 
-        # Permute cluster memberships:
+        # Permute cluster membership that minimizes objective the most:
         # (a) Compute M = ~SA
         M = S_bar @ A
 
         # (b) Compute v
         MoA = np.multiply(M, A)
-        #v = [min([M[i][j] - sum([MoA[i][j]]) for j in range(k)]) for i in range(N)]
         v = [min([M[i][j] for j in range(k)]) - sum([MoA[i][j] for j in range(k)]) for i in range(N)]
 
         # Check if we converged
@@ -81,7 +103,11 @@ def cluster(S, k=None, max_iter=100):
         C = np.argmin([M[X][j] for j in range(k)])
         A[X] = np.zeros((k))
         A[X][C] = 1
+
         progress(_i, max_iter)
+        
+        if visualize: 
+            plot(points, A, k)
 
     return A
 
@@ -104,6 +130,9 @@ def square(S):
 
 
 def read_points(from_file): 
+    """
+    Reads and returns a list of points [(x,y), ...] from a file.
+    """
     points = []
     with open(from_file) as fp: 
         for line in fp.readlines(): 
@@ -113,38 +142,30 @@ def read_points(from_file):
     return points
 
 
-def similarity_matrix(P):
+def similarity_matrix(P, similarity_measure):
+    """
+    Builds a similarity matrix over a set of elements
+    using the provided similarity measure. 
+    """
     N = len(P) 
     S = np.zeros((N, N))
     for i in range(N): 
         for j in range(i): 
-            S[i][j] = euclidean_distance(P[i], P[j])
+            S[i][j] = similarity_measure(P[i], P[j])
 
     S = square(S)
     S = S / np.max(S)
-    S = 1 - S # Higher value = more similar
+    S = 1 - S  # Higher value = more similar
 
     return S
 
 
-def test(): 
+def demo(): 
     print(f'Reading...')
-    points = read_points('data/synth')
+    points = read_points('data/clusters')
     print(f'Calculating similarity matrix...')
-    S = similarity_matrix(points)
+    S = similarity_matrix(points, similarity_measure=euclidean_distance)
     print(f'Clustering...')
-    A = cluster(S, k=50, max_iter=5000)
+    A = cluster(S, k=10, max_iter=1000, visualize=True, points=points)
 
-    # Visualise
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    labels = [np.argmax(p) for p in A]
-    xs, ys = zip(*points)
-    ax.scatter(xs, ys, c=labels)
-    plt.show()
-
-
-
-# cluster(np.random.random((10, 10)), k=5)
-test()
-
+demo()
